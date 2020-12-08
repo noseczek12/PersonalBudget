159cda60e00a47f1c0f987804fc05fea08696c40
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2020-12-08 22:12:57 +0100

    Add new classes: BudgetManager and Usermanager

---
+++ BudgetManager.h
@@ -0,0 +1,18 @@
+#ifndef BUDGETMANAGER_H
+#define BUDGETMANAGER_H
+
+#include <iostream>
+//#include "UzytkownikMenedzer.h"
+//#include "AdresatMenedzer.h"
+//#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class BudgetManager {
+
+public:
+
+};
+
+
+#endif
---
+++ UserManager.h
@@ -0,0 +1,18 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <iostream>
+//#include "UzytkownikMenedzer.h"
+//#include "AdresatMenedzer.h"
+//#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class UserManager {
+
+public:
+
+};
+
+
+#endif
