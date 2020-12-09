d71901c4b08bd2a9e94aa65f1dceea43205cb444
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2020-12-09 09:46:55 +0100

    Add methods to BudgetManager class

--- BudgetManager.cpp
+++ BudgetManager.cpp
@@ -1,1 +1,36 @@
 #include "BudgetManager.h"
+
+Income BudgetManager::enterNewIncomeData()
+{
+
+}
+
+Expense BudgetManager::enterNewExpenseData()
+{
+
+}
+
+char BudgetManager::chooseOptionFromAdditionalMenu()
+{
+
+}
+
+void BudgetManager::addIncome()
+{
+
+}
+
+void BudgetManager::addExpense()
+{
+
+}
+
+void BudgetManager::currentMonthBalance()
+{
+
+}
+
+void BudgetManager::chosenPeriodBalance()
+{
+
+}
--- BudgetManager.h
+++ BudgetManager.h
@@ -2,6 +2,7 @@
 #define BUDGETMANAGER_H
 
 #include <iostream>
+#include <vector>
 //#include "UzytkownikMenedzer.h"
 //#include "AdresatMenedzer.h"
 //#include "MetodyPomocnicze.h"
@@ -9,9 +10,20 @@
 using namespace std;
 
 class BudgetManager {
-
+    int ID_OF_LOGGED_IN_USER;
+    vector <Income> incomes;
+    vector <Expense> expenses;
+    //FileWithExpenses fileWithExpenses;
+    //FileWithIncomes fileWithIncomes;
+    Income enterNewIncomeData();
+    Expense enterNewExpenseData();
+    char chooseOptionFromAdditionalMenu();
 public:
-
+    BudgetManager(){};
+    void addIncome();
+    void addExpense();
+    void currentMonthBalance();
+    void chosenPeriodBalance();
 };
 
 
