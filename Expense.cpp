0e6904022fddb86ec97ca25378d7c3beba2405a7
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2020-12-14 18:33:19 +0100

    Add new Expense class with methods

---
+++ Expense.cpp
@@ -0,0 +1,43 @@
+#include "Expense.h"
+
+void Expense::setExpenseId(int newExpenseId)
+{
+    expenseId = newExpenseId;
+}
+void Expense::setUserId(int newUserId)
+{
+    userId = newUserId;
+}
+void Expense::setDate(string newDate)
+{
+    date = newDate;
+}
+void Expense::setItem(string newItem)
+{
+    item = newItem;
+}
+void Expense::setAmount(string newAmount)
+{
+    amount = newAmount;
+}
+
+int Expense::getExpenseId()
+{
+    return expenseId;
+}
+int Expense::getUserId()
+{
+    return userId;
+}
+string Expense::getDate()
+{
+    return date;
+}
+string Expense::getItem()
+{
+    return item;
+}
+string Expense::getAmount()
+{
+    return amount;
+}
